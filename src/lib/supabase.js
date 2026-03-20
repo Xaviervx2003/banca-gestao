8a66baa6bd121deb938eb6fddc79e36b532bda8e
Xaviervx2003 <ALOK.DJ.GOD@GMAIL.COM>
2026-03-20 18:10:46 -0400

    fix supabase key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL  = import.meta.env.VITE_SUPABASE_URL  || 'https://ueusymlsintmhrmvtxmt.supabase.co';
-const SUPABASE_KEY  = import.meta.env.VITE_SUPABASE_KEY  || 'sb_publishable__P-oslN7wbDrNzbGoOSW7Q_uT-tSEPXueusymlsintmhrmvtxmt';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://ueusymlsintmhrmvtxmt.supabase.co';
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVldXN5bWxzaW50bWhybXZ0eG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwNDA2MjYsImV4cCI6MjA4OTYxNjYyNn0.1JldrUNiWaWTT25DnJcumbH_TXXj9fk13ceGP5_mZNs';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
